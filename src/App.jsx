e58323f3a26445064b4afe42bb441ccc8d40879d
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-09-25 21:57:14 -0600

    QR UI añadida

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import DefaultNavbar from './components/NavBar'
 import CreateCardGeneral from './pages/CreateCardGeneral'
 import Login from './pages/Login'
 import ShareCard from './pages/ShareCard'
+import QR from './pages/QR'
 
 function App () {
   return (
     <div>
       <DefaultNavbar />
-      <Login />
+      <QR />
     </div>
   )
 }
