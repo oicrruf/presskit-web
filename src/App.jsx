cdf0144c249ba3b5b58c5b2ace97160273da768a
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-09-25 19:13:50 -0600

    🩹 changes as PR review

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,6 @@ import CreateCardGeneral from './pages/CreateCardGeneral'
 function App () {
   return (
     <div>
-      {/* <img src='./qr_logo.png' alt='Logo' className='w-3/12' />
-      <div className='container mx-auto bg-grey-dark rounded-xl shadow border p-8 m-10'>
-        <h1 className='text-3xl text-white font-bold mb-5'>
-          Presskit App
-        </h1>
-        <p className='text-white text-lg'>
-          Base Project
-        </p>
-      </div> */}
       <DefaultNavbar />
       <CreateCardGeneral />
     </div>
