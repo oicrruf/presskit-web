fe7b7dccb4375cc234ad2e64472dd0ca19aa5e28
GerardoPuertosSada <gerardopsada@gmail.com>
2023-09-25 20:30:12 -0600

    diseño final

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,59 @@
-function App () {
+const Login = () => {
   return (
-    <div className='bg-gradient-to-t from-black-medium to-grey-dark h-screen flex flex-col items-center'>
-      <img src='./qr_logo.png' alt='Logo' className='w-3/12' />
-      <div className='container mx-auto bg-grey-dark rounded-xl shadow border p-8 m-10'>
-        <h1 className='text-3xl text-white font-bold mb-5'>
-          Presskit App
-        </h1>
-        <p className='text-white text-lg'>
-          Base Project
-        </p>
+    <>
+      <div className='bg-grey-dark min-h-screen flex flex-col'>
+
+        {/* Sección superior */}
+        <section className='bg-grey-light p-4 text-center'>
+          <div>
+            Logo
+          </div>
+          <div className='mt-4'>
+            <h2>
+              ¡Crea tu tarjeta de presentación electrónica en segundos! Ingrese
+              sus datos y obtenga una tarjeta elegante con código QR para
+              compartir su contacto de manera rápida y efectiva!
+            </h2>
+          </div>
+          <div className='mt-4'>
+            <button className='btn-contact bg-white rounded-lg px-5 py-1'>
+              Contact
+            </button>
+          </div>
+        </section>
+
+        {/* Sección inferior */}
+        <section className='bg-grey-lighter p-4'>
+          <section className='bg-grey rounded-lg p-4'>
+            <div className='my-3 text-center'>
+              <h2>¿Ya tienes una cuenta?</h2>
+            </div>
+            <div>
+              <h3>Usuario</h3>
+              <input
+                className='rounded-lg mb-4 w-full'
+                type='text'
+                placeholder='Email'
+              />
+            </div>
+            <div>
+              <h3>Contraseña</h3>
+              <input
+                className='rounded-lg w-full'
+                type='password'
+                placeholder='Password'
+              />
+            </div>
+          </section>
+          <div className='text-center mt-4'>
+            <button className='btn-login bg-white rounded-lg px-5 py-1'>
+              Login
+            </button>
+          </div>
+        </section>
       </div>
-    </div>
+    </>
   )
 }
 
-export default App
+export default Login
