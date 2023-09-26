5a07a2140956819e0785c388f419fa92f2967cb3
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-09-25 21:50:53 -0600

    Cambios en estilos de login siguiendo figma

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import DefaultNavbar from './components/NavBar'
 import CreateCardGeneral from './pages/CreateCardGeneral'
 import Login from './pages/Login'
+import ShareCard from './pages/ShareCard'
 
 function App () {
   return (
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,22 +3,22 @@ import logo from '../assets/Logo_160x160.png'
 const Login = () => {
   return (
     <>
-      <div className='bg-grey-dark min-h-screen flex flex-col'>
+      <div className='bg-black-medium min-h-screen flex flex-col'>
 
         {/* Sección superior */}
         <section className='bg-grey-light p-4 text-center'>
           <div className='flex items-center justify-center'>
-            <img src={logo} alt="Logo" />
+            <img src={logo} alt='Logo' />
           </div>
           <div className='mt-4'>
-            <h2>
+            <h2 className='text-white'>
               ¡Crea tu tarjeta de presentación electrónica en segundos! Ingrese
               sus datos y obtenga una tarjeta elegante con código QR para
               compartir su contacto de manera rápida y efectiva!
             </h2>
           </div>
           <div className='mt-4'>
-            <button className='btn-contact bg-white rounded-lg px-5 py-1'>
+            <button className='bg-grey w-[240px] h-[45px] rounded-[20px] text-xl mb-5 text-black'>
               Contact
             </button>
           </div>
@@ -26,14 +26,14 @@ const Login = () => {
 
         {/* Sección inferior */}
         <section className='bg-grey-lighter p-4'>
-          <section className='bg-grey rounded-lg p-4'>
+          <section className='bg-grey rounded-[20px] p-4'>
             <div className='my-3 text-center'>
               <h2>¿Ya tienes una cuenta?</h2>
             </div>
             <div>
               <h3>Usuario</h3>
               <input
-                className='rounded-lg mb-4 w-full'
+                className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full'
                 type='text'
                 placeholder='Email'
               />
@@ -41,14 +41,14 @@ const Login = () => {
             <div>
               <h3>Contraseña</h3>
               <input
-                className='rounded-lg w-full'
+                className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full'
                 type='password'
                 placeholder='Password'
               />
             </div>
           </section>
           <div className='text-center mt-4'>
-            <button className='btn-login bg-white rounded-lg px-5 py-1'>
+            <button className='bg-green w-[240px] h-[45px] rounded-[20px] text-xl mb-5 text-black'>
               Login
             </button>
           </div>
