ea2bfb37a484e9b61f32f46c21e7e378976074f0
jabooo188 <secretoadivina01@gmail.com>
2023-09-25 21:55:30 -0600

    Cambio nombre componente

---
+++ src/pages/PrincipalPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PrincipalPage = () => {
+  return (
+    <div className='justify-content-stretch'>
+      <button type='submit' className='bg-black text-white rounded-lg w-80 m-1.5 text-4xl'>Login</button>
+      <button type='submit' className='bg-black text-white rounded-lg w-80 m-1.5 text-4xl'>Register</button>
+    </div>
+  )
+}
+
+export default PrincipalPage
--- src/pages/inicio.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const inicio = () => {
-  return (
-    <div>inicio</div>
-  )
-}
-
-export default inicio
