c4af72f16c473937d0cde1dd43e15a52233bd0f0
jabooo188 <secretoadivina01@gmail.com>
2023-09-27 19:53:44 -0600

    Pagina inicio

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,8 @@ import PrincipalPage from './pages/PrincipalPage'
 
 function App () {
   return (
-    <div className='bg-gradient-to-t from-black-medium to-grey-dark h-screen flex flex-col items-center'>
-      <img src='./qr_logo.png' alt='Logo' className='w-3/12' />
-      <div className='container mx-auto bg-grey-dark rounded-xl shadow border p-8 m-10'>
-        <div className='justify-center'>
-          <PrincipalPage />
-        </div>
-      </div>
+    <div>
+      <PrincipalPage />
     </div>
   )
 }
--- src/pages/PrincipalPage.jsx
+++ src/pages/PrincipalPage.jsx
@@ -2,9 +2,25 @@ import React from 'react'
 
 const PrincipalPage = () => {
   return (
-    <div className='justify-content-stretch'>
-      <button type='submit' className='bg-black text-white rounded-lg w-80 m-1.5 text-4xl'>Login</button>
-      <button type='submit' className='bg-black text-white rounded-lg w-80 m-1.5 text-4xl'>Register</button>
+    <div>
+      <div className='bg-gradient-to-t from-black-medium to-grey-dark h-screen flex flex-col items-center'>
+        <img src='./qr_logo.png' alt='Logo' className='w-3/12' />
+        <div className='text-white text-xl m-10'>
+          <p>
+            ¡Crea tu tarjeta de presentación electrónica
+            en segundos! Ingresa tus datos y obtén
+            una tarjeta elegante con código QR para
+            compartir tu contacto de manera rápida
+            y efectiva!
+          </p>
+        </div>
+        <div className='container bg-grey-dark rounded-xl shadow border p-8 m-10 max-w-100% d-flex justify-content-center grid-rows-1'>
+          <div className=''>
+            <button type='submit' className='bg-black text-white rounded-lg w-80 m-1.5 text-4xl'>Login</button>
+            <button type='submit' className='bg-black text-white rounded-lg w-80 m-1.5 text-4xl'>Register</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
