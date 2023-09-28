f34fa5cd71b7ed8062e3d716ca08e9171be3cf1a
JoseMiguelVazquez <jmiguel_1991@hotmail.com>
2023-09-27 19:50:15 -0600

    fixed responsiveness and add vector logo

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,9 @@
+import logo from '../assets/Logo_160x160.png'
+
 const Register = () => {
   return (
-    <div className='bg-gradient-to-t from-black-medium to-grey-dark w-screen mx-auto flex flex-col items-center'>
-      <img src='./qr_logo.png' alt='Logo' className='my-20 w-3/12' />
+    <div className='bg-black-medium w-screen mx-auto flex flex-col items-center'>
+      <img src={logo} alt='Logo' />
       <div className='w-2/2 bg-black rounded-xl shadow border p-5 m-4'>
         <h1 className='text-4xl text-white font-bold text-center bg-black'>
           Compartenos tu
@@ -9,52 +11,54 @@ const Register = () => {
         </h1>
 
       </div>
-      <div className='bg-grey rounded-[20px] p-4 my-10 w-80 flex flex-col items-center'>
-        <label className='block w-full'>
-          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
-            Nombre
-          </span>
-          <input type='nombre' name='nombre' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='Nombre' />
-        </label>
+      <div className='w-full p-4'>
+        <div className='bg-grey rounded-[20px] p-4 my-10 flex flex-col items-center'>
+          <label className='block w-full'>
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
+              Nombre
+            </span>
+            <input type='nombre' name='nombre' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='Nombre' />
+          </label>
 
-        <label className='block w-full'>
-          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
-            Apellido
-          </span>
-          <input type='apellido' name='apellido' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='Apellido' />
-        </label>
+          <label className='block w-full'>
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
+              Apellido
+            </span>
+            <input type='apellido' name='apellido' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='Apellido' />
+          </label>
 
-        <label className='block w-full'>
-          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
-            Correo
-          </span>
-          <input type='Email' name='email' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='@gmail.com' />
-        </label>
+          <label className='block w-full'>
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
+              Correo
+            </span>
+            <input type='Email' name='email' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='@gmail.com' />
+          </label>
 
-        <label className='block w-full'>
-          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
-            Teléfono
-          </span>
-          <input type='Phone' name='Phone' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='' />
-        </label>
+          <label className='block w-full'>
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
+              Teléfono
+            </span>
+            <input type='Phone' name='Phone' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='' />
+          </label>
 
-        <label className='block w-full'>
-          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
-            Usuario
-          </span>
-          <input type='User' name='User' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='Usuario' />
-        </label>
+          <label className='block w-full'>
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
+              Usuario
+            </span>
+            <input type='User' name='User' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='Usuario' />
+          </label>
 
-        <label className='block w-full'>
-          <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
-            Password
-          </span>
-          <input type='Password' name='Password' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='Password' />
-        </label>
+          <label className='block w-full'>
+            <span className="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm my-4 font-medium text-slate-800">
+              Password
+            </span>
+            <input type='Password' name='Password' className='bg-grey-medium rounded-[20px] mb-5 h-[40px] placeholder:text-white placeholder:ps-5 text-white w-full' placeholder='Nombre' placeholder='Password' />
+          </label>
 
-        <button className='bg-green w-[240px] h-[45px] rounded-[20px] text-xl mb-5 text-black'>
-          Registrate
-        </button>
+          <button className='bg-green w-[240px] h-[45px] rounded-[20px] text-xl mb-5 text-black'>
+            Registrate
+          </button>
+        </div>
       </div>
     </div>
   )
